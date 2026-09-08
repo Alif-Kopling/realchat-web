81f65258f722aa64d67820202e2bb4484e4f876e
Alif-Kopling <2425307.alif@smkn-2sbg.sch.id>
2026-09-08 13:24:57 +0700

    fix(strings): standardize app name casing in strings.xml and capacitor.config.ts fix(package): update version to 1.0.0 in package-lock.json

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.hallowok.app',
-  appName: 'Hallowok Beta Version',
+  appName: 'hallowok',
   webDir: 'dist',
   server: {
     androidScheme: 'https',
